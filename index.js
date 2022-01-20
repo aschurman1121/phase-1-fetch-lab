a7b345bea651428da89ca56fff7444a012fc29d5
alexandra <alexandraschurman@gmail.com>
2022-01-19 19:33:30 -0800

    commit tree e058832d129cc5dfe2646410215b1fe92fe546bf

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+
 function fetchBooks() {
+  
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then((resp) => resp.json())
+  .then((potato) => renderBooks(potato));
+  
+  // console.log(fetchBooks);
+  // console.log(books); 
+  // renderBooks(Object.title)
+}
+
+function renderBooks(books) {
+  const main = document.querySelector('main');
+  books.forEach(book => {
+    const h2 = document.createElement('h2');
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
   
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
+
+
+
+
+
+/*function fetchBooks("https://anapioficeandfire.com/api/books") {
+
+
+
+  // To pass the tests, don't forget to return your fetch!
+  .then((resp) => resp.json())
+  .then((json) => console.log(json));}
+  
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +50,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+*/
